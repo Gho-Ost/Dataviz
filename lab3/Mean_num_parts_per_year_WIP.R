e2fca67f9206a1e265c2704390fc98202e52f5c9
MatTheTab <109167054+MatTheTab@users.noreply.github.com>
2023-04-07 21:54:19 +0200

    Update and add minor changes

--- lab3/Mean_num_parts_per_year_WIP.R
+++ lab3/Mean_num_parts_per_year_WIP.R
@@ -8,14 +8,11 @@ avg_parts_per_year <- merged_df %>%
   group_by(year) %>%
   summarize(avg_parts = mean(num_parts))
 
-library(ggplot2)
-
+#First Plot
 ggplot(data = avg_parts_per_year, aes(x = year, y = avg_parts)) +
   geom_line(size = 1) +
-  labs(x = "Year", y = "Average number of parts per set")
-
-# number per set
-library(dplyr)
+  labs(x = "Year", y = "Average Number of Parts per Set") +
+  ggtitle("Average Number of Parts per Set per Year")
 
 # Create a data frame with the average number of parts per theme
 theme_part_counts <- merged_df %>%
@@ -32,16 +29,34 @@ theme_part_counts <- theme_part_counts %>%
 top_20_avg_parts <- head(theme_part_counts %>% arrange(desc(avg_parts)),20)
 top_20_max_parts <- head(theme_part_counts %>% arrange(desc(max_parts)),20)
 
-# Dots are optional, delete geom_point() if you do not want them
-
+# Second Plot
+# Version A with Dots
 ggplot(top_20_avg_parts, aes(x = reorder(name.y, avg_parts), y = avg_parts)) +
   geom_bar(stat = "identity") +
   coord_flip() +
   labs(x = "Theme", y = "Average Number of Parts") +
-  geom_point(aes(reorder(name.y, avg_parts),y=max_parts))
+  geom_point(aes(reorder(name.y, avg_parts),y=max_parts)) +
+  ggtitle("Average and Maximum Number of Parts in Themes for Chosen Themes")
+# Version B without Dots
+ggplot(top_20_avg_parts, aes(x = reorder(name.y, avg_parts), y = avg_parts)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(x = "Theme", y = "Average Number of Parts in a Set") +
+  ggtitle("Average Number of Parts in Themes for Chosen Themes")
 
+
+#Third Plot with Dots
+# Version B with Dots
 ggplot(top_20_max_parts, aes(x = reorder(name.y, avg_parts), y = avg_parts)) +
   geom_bar(stat = "identity") +
   coord_flip() +
-  labs(x = "Theme", y = "Average Number of Parts") +
-  geom_point(aes(reorder(name.y, avg_parts),y=max_parts))
+  labs(x = "Theme", y = "Number of Parts in a Set") +
+  geom_point(aes(reorder(name.y, avg_parts),y=max_parts)) +
+  ggtitle("Average and Maximum Number of Parts in Themes for Chosen Themes")
+# Version B without Dots
+ggplot(top_20_max_parts, aes(x = reorder(name.y, avg_parts), y = avg_parts)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(x = "Theme", y = "Number of Parts in a Set") +
+  ggtitle("Average Number of Parts in Themes for Chosen Themes")
+
