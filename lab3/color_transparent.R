710519ef878224cb1196949daab50d659fccf985
Gho-Ost <100212265+Gho-Ost@users.noreply.github.com>
2023-04-07 11:47:51 +0200

    Added transparent no legend version

--- lab3/color_transparent.R
+++ lab3/color_transparent.R
@@ -19,3 +19,19 @@ trans_count %>%
         legend.title = element_text(size=20, color="#d1d1d1")) +
   guides(fill=guide_legend(title="Is transparent  "))
 
+# No legend
+trans_count %>% 
+  ggplot(aes(x="", y=count, fill=is_transparent, label=proportions)) + 
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start = 0) + 
+  ggtitle("Proportion of transparent colors") +
+  theme_void() +
+  geom_text(nudge_y = c(-45,-19), nudge_x = c(0.0025,0.2), size=9, color="white", fontface="bold") +
+  theme(plot.title = element_text(hjust=-1.75, vjust=0.1, color="#d1d1d1", size=25),
+        plot.background = element_rect(fill="#2d2d2d"),
+        axis.ticks.length = unit(0, "pt"),
+        legend.position = "none") +
+  guides(fill=guide_legend(title="Is transparent  ")) + 
+  annotate(geom="text", x=1.8, y=20, label="Transparent", size=7, color="white") + 
+  annotate(geom="text", x=1.8, y=125, label="Non-transparent", size=7, color="white")
+
